e8ded11f6cc29b7a02666225c6da01ef5df80c92
Marc Esquerra <esquerra@gmail.com>
2021-09-09 19:25:03 +0100

    WiP

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -6,12 +6,13 @@ object Checksum {
     System.out.print("\u001b[H\u001b[2J")
     System.out.flush()
 
-  def main(args: Array[String]): Unit = {
-    for _ <- 1 to 15 do
+  def main(args: Array[String]): Unit =
+    for i <- 1 to 15 do
       clear()
+      println(s"[$i/15]")
       print("> ")
       val firstName = readLine()
       println()
       println(firstName)
-  }
+
 }
